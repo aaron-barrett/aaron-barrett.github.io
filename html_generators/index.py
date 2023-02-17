c7717f09ee142bef3dba0b19a26cabc5827e6304
Aaron Barrett <abarrett471@gmail.com>
2023-02-17 02:25:38 -0600

    adjusted homepage

--- html_generators/index.py
+++ html_generators/index.py
@@ -19,7 +19,7 @@ with tag('html', lang="en"):
             with tag('h1'):
                 text("Welcome to my Website!")
             with tag('div', id = "main_page"):
-                text("Welcome to my website! The only tab that will contain content is the math tab, and it does not have much, perhaps nothing. This is intentional. The goal of this website is to talk about things that interest me such as mathematics, music theory, and possibly programming, and the focus will be mathematics, for now. While this could be thought of as a display of self-promotion, it is not self-aggrandizing; this is not a blog, and it never will be.")
+                text("Welcome! The only tab that contains content is the math tab, and it does not have much, perhaps nothing. This is intentional. The goal of this website is to talk about things that interest me such as mathematics, music theory, and possibly programming. The focus will be mathematics, for now. While this could be thought of as a display of self-promotion, it is not self-aggrandizing; this is not a blog, and it never will be.")
 f = open("index.html",'w')
 print(indent(doc.getvalue()), file=f)
 # print(doc.getvalue())
