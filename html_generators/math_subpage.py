f1fe11efc4ac389ecab443dde26ee4697098bdb6
Aaron Barrett <abarrett471@gmail.com>
2023-02-18 03:05:09 -0600

    mathsubpage - change to mathjax

--- html_generators/math_subpage.py
+++ html_generators/math_subpage.py
@@ -18,10 +18,12 @@ with tag('html', lang="en"):
         doc.stag('link', rel="stylesheet", href="styles.css")
         with tag("title"):
             text("Math")
-        doc.stag('link', rel='stylesheet', href='https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css', integrity='sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0', crossorigin='anonymous')
-        with tag('script', 'defer', src='https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js', integrity='sha384-PwRUT/YqbnEjkZO0zZxNqcxACrXe+j766U2amXcgMg5457rve2Y7I6ZJSm2A0mS4', crossorigin='anonymous'):
-            text("")
-        with tag("script", "defer", src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/contrib/auto-render.min.js", integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05", crossorigin="anonymous", onload="renderMathInElement(document.body);"):
+        # doc.stag('link', rel='stylesheet', href='https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css', integrity='sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0', crossorigin='anonymous')
+        # with tag('script', 'defer', src='https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js', integrity='sha384-PwRUT/YqbnEjkZO0zZxNqcxACrXe+j766U2amXcgMg5457rve2Y7I6ZJSm2A0mS4', crossorigin='anonymous'):
+        #     text("")
+        # with tag("script", "defer", src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/contrib/auto-render.min.js", integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05", crossorigin="anonymous", onload="renderMathInElement(document.body);"):
+        #     text("")
+        with tag("script", "async", src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js", type="text/javascript", id="MathJax-script"):
             text("")
     with tag('div', klass = "topnav"):
         with tag('a', href="index.html"):
