87e17790418960a933403a4c88c9cddea97554db
Aaron Barrett <abarrett471@gmail.com>
2023-02-18 23:45:16 -0600

    subpage tex - small update

--- math/generate_math_subpages_html.py
+++ math/generate_math_subpages_html.py
@@ -7,5 +7,5 @@ if (os.path.basename(os.getcwd()) != "math"):
 dir_list = [ f for f in os.listdir("tex") if f.endswith(".tex") and "header" not in f]
 for f in dir_list:
     name = os.path.splitext(f)
-    os.system("pandoc tex/" + f + " --mathjax -o html/" + name[0] + ".html")
+    os.system("pandoc tex/" + f + " --mathjax -o pandoc_html/" + name[0] + ".html")
     
